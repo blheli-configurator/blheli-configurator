20eecc468a8dfe7b66983e4bd143f01b80ad2617
DieHertz <diehertz@gmail.com>
2017-01-05 19:13:15 +0300

    Added new 16.53 revision with fixed NAME

--- js/blheli_versions.js
+++ js/blheli_versions.js
@@ -7,9 +7,9 @@ var BLHELI_S_SILABS_VERSIONS = [
 		key: '16.5'
 	},
 	{
-		version: '16.53', name: '16.53', commit: 'b8e9e44fe118355876653179e5eb31493e525ab2',
+		version: '16.53', name: '16.53', commit: '1bf3d915f67e838b9985c0e61b7b838966838184',
 		url: 'https://raw.githubusercontent.com/cleanflight/blheli-multishot/{0}/BLHeli_S SiLabs/Hex files/{1}_REV{3}.HEX',
-		key: '16.53MS_'
+		key: '16.53MS__'
 	},
 	{
 		version: '16.45', name: '16.45', commit: 'e8b56843a95bc2ba217680df341a716250730c74',
