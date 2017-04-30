f055c2f7ed35072b40aff50be0887243f79e3817
DieHertz <diehertz@gmail.com>
2017-04-30 16:24:45 +0300

    Added persistent logging

--- main.js
+++ main.js
@@ -1,29 +1,83 @@
 'use strict';
 
+function debounce(func, wait, immediate) {
+    // 'private' variable for instance
+    // The returned function will be able to reference this due to closure.
+    // Each call to the returned function will share this common timer.
+    var timeout;
+
+    // Calling debounce returns a new anonymous function
+    return function() {
+        // reference the context and args for the setTimeout function
+        var context = this, 
+            args = arguments;
+
+        // Should the function be called now? If immediate is true
+        //   and not already in a timeout then the answer is: Yes
+        var callNow = immediate && !timeout;
+
+        // This is the basic debounce behaviour where you can call this 
+        //   function several times, but it will only execute once 
+        //   [before or after imposing a delay]. 
+        //   Each time the returned function is called, the timer starts over.
+        clearTimeout(timeout);   
+
+        // Set the new timeout
+        timeout = setTimeout(function() {
+
+             // Inside the timeout function, clear the timeout variable
+             // which will let the next execution run when in 'immediate' mode
+             timeout = null;
+
+             // Check if the function already ran with the immediate flag
+             if (!immediate) {
+               // Call the original function with apply
+               // apply lets you define the 'this' object as well as the arguments 
+               //    (both captured before setTimeout)
+               func.apply(context, args);
+             }
+        }, wait);
+
+        // Immediate mode and no wait timer? Execute the function..
+        if (callNow) func.apply(context, args);
+     }; 
+}
+
 // Fast way to log possible errors and uncaught exceptions
 window.console = (function (origConsole) {
     var data = [],
         isDebug = false;
 
+    getFromLocalStorage('log')
+    .then(prevLog => data.push(prevLog))
+    .done();
+
+    var writeDebounced = debounce(() => setToLocalStorage('log', data.join('\n')), 250, true);
+
     return {
         log: function() {
             data.push(Array.prototype.slice.call(arguments).join(' '));
+            writeDebounced();
             origConsole.log.apply(origConsole, arguments);
         },
         warn: function() {
             data.push(Array.prototype.slice.call(arguments).join(' '));
+            writeDebounced();
             origConsole.warn.apply(origConsole, arguments);
         },
         error: function() {
             data.push(Array.prototype.slice.call(arguments).join(' '));
+            writeDebounced();
             origConsole.error.apply(origConsole, arguments);
         },
         info: function() {
             data.push(Array.prototype.slice.call(arguments).join(' '));
+            writeDebounced();
             origConsole.info.apply(origConsole, arguments);
         },
         debug: function() {
             data.push(Array.prototype.slice.call(arguments).join(' '));
+            writeDebounced();
             isDebug && origConsole.debug.apply(origConsole, arguments);
         },
         setDebug: function(b) { isDebug = b },
